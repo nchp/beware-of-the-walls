180d8b557e7f7150a476395c91d6a66aedbe30a3
Nicha Limmanee <nicha.lim@ku.th>
2017-10-16 13:11:03 +0700

    Running init walls.

--- walls.py
+++ walls.py
@@ -40,6 +40,7 @@ class WallsWindow(arcade.Window):
 #        self.block_sprite = ModelSprite('images/block.png',model=self.world.block)
 #        self.walls_sprite = ModelSprite('images/walls.png')
         
+        self.init_walls_list = arcade.SpriteList()
         self.walls_list = arcade.SpriteList()
 
         for i in range(3):
@@ -48,16 +49,16 @@ class WallsWindow(arcade.Window):
                     self.walls = arcade.Sprite("images/walls.png")
                     self.walls.center_x = WALLS_X_POSITION[k]
                     self.walls.center_y = ((i+j+3)*100)-50
-                    self.walls_list.append(self.walls)
+                    self.init_walls_list.append(self.walls)
                 for l in range(i+1):
                     self.walls = arcade.Sprite("images/walls.png")
                     self.walls.center_x = WALLS_X_POSITION[6-l]
                     self.walls.center_y = ((i+j+3)*100)-50
-                    self.walls_list.append(self.walls)
+                    self.init_walls_list.append(self.walls)
 
     def draw_initial_page(self):
 #        self.block_sprite.draw()
-        self.walls_list.draw()
+        self.init_walls_list.draw()
         self.dot_sprite.draw()
 
         arcade.draw_text("Click the dot to start playing.",
@@ -67,8 +68,9 @@ class WallsWindow(arcade.Window):
     def draw_game(self):
         self.set_mouse_visible(False)
 #        self.block_sprite.draw()
-        self.walls_list.draw()
-        self.dot_sprite.draw() 
+        self.init_walls_list.draw()
+        self.dot_sprite.draw()
+        #self.dot_sprite.center_x = 300
 
         minutes = int(self.total_time) // 60
         seconds = int(self.total_time) % 60
@@ -76,14 +78,16 @@ class WallsWindow(arcade.Window):
         if not self.timer_text or self.timer_text.text != output:
             self.timer_text = arcade.create_text(output, arcade.color.WHITE, 20)
         arcade.render_text(self.timer_text, self.width - 80, self.height - 40)
+            
 
     def draw_game_over(self):
+        self.dot_sprite.draw()
         self.set_mouse_visible(True)
         output = "Game Over"
-        arcade.draw_text(output, 240, 400, arcade.color.WHITE, 54)
+        arcade.draw_text(output, 150, 400, arcade.color.WHITE, 24)
 
         output = "Click to restart"
-        arcade.draw_text(output, 310, 300, arcade.color.WHITE, 24)    
+        arcade.draw_text(output, 110, 300, arcade.color.WHITE, 24)    
 
     def on_draw(self):
         arcade.start_render()
@@ -94,17 +98,48 @@ class WallsWindow(arcade.Window):
         elif self.current_state == GAME_RUNNING:
             self.draw_game()
 
-        else:
-            self.draw_game()
+        elif self.current_state == GAME_OVER:
             self.draw_game_over()
+
+#        else:
+#            self.draw_game()
+#            self.draw_game_over()
 
     def update(self, delta):
         if self.current_state == GAME_RUNNING:
             self.world.update(delta)
 #            self.block_sprite.set_position(self.world.block.x, self.world.block.y)
 
-        
-
+            for self.walls in self.init_walls_list:
+                self.walls.center_y -= 2
+                if self.walls.top < 0:
+                    self.walls.kill()
+                
+                if arcade.check_for_collision_with_list(self.dot_sprite,self.init_walls_list):
+                    #self.dot_sprite.center_x = 210
+                    self.current_state = GAME_OVER
+            """
+            self.last_road = 3
+            self.last_y = 750
+            up = random.randrange(1)
+            if up:
+                for i in range(6):
+                    if i == last_road:
+                        self.last_road = i
+                        continue
+                    self.walls = arcade.Sprite("images/walls.png")
+                    self.walls.center_x = WALLS_X_POSITION[i]
+                    self.walls.center_y = self.last_y
+                    self.walls_list.append(self.walls)
+                walls.list.draw()
+#            else:
+                
+            
+            for self.walls in self.walls_list:
+                self.walls.center_y -= 2
+                if self.walls.top < 0:
+                    self.walls.kill()
+            """
         self.total_time += delta
 
     def on_mouse_press(self, x, y, button, modifiers):
